54eaa3872eaf587092d4f279b77ccdf4074fb8fb
israel2x <ezurita@fiec.espol.edu.ec>
2025-04-28 16:37:44 -0500

    Clase Intro Controladores - 23 de Abril

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <nav>
+        <a href="/">Inicio</a>
+        <a href="/notas">Notas</a>
+        <a href="/matricula">Matricula</a>
+        <a href="/editar">Editar Notas</a>
+    </nav>
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/notas', function () {
+    return view('notas');
+});
+
+Route::get('/matricula', function () {
+    return view('matricula');
+});
+
+
+Route::get('/editar', function () {
+    return view('editarnotas');
+});
