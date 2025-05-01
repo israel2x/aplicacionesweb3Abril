6fca62c9fb9227f65471bcfec739946461c4f4ed
israel2x <ezurita@fiec.espol.edu.ec>
2025-04-30 19:03:56 -0500

    Clase 30 de abril - controlador basico

--- app/Http/Controllers/NotasController.php
+++ app/Http/Controllers/NotasController.php
@@ -6,5 +6,20 @@ use Illuminate\Http\Request;
 
 class NotasController extends Controller
 {
-        
+       public function index() {
+            return view("notas.mostrar");
+       }
+
+       public function crear() {
+            return view("notas.crear");
+       }
+
+       public function editar() {
+        return view("notas.editar");
+       }
+
+       public function eliminar() {
+        return view("notas.eliminar");
+       }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\NotasController;
 
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/notas', function () {
+/* Route::get('/notas', function () {
     return view('notas');
 });
 
@@ -18,4 +19,9 @@ Route::get('/matricula', function () {
 
 Route::get('/editar', function () {
     return view('editarnotas');
-});
+}); */
+
+Route::get('/mostrar', [NotasController::class, 'index']);
+Route::get('/crear', [NotasController::class, 'crear']);
+Route::get('/editar', [NotasController::class, 'editar']);
+Route::get('/eliminar', [NotasController::class, 'eliminar']);
